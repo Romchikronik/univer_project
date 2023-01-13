1adae059881d18a2e951a4c8b89f99a7015cf776
Romchik <romankolesov44@gmail.com>
2023-01-13 09:52:39 +0500

    update burger btn without js

--- src/js/script.js
+++ src/js/script.js
@@ -3,12 +3,9 @@ $(document).ready(function () {
     if (!$(this).hasClass("open")) {
       $(this).addClass("open");
       $("html").css("overflow-y", "hidden");
-      $(".nav-menu").slideDown(500);
-      $(".nav-menu").addClass("fixed-top");
     } else {
       $(this).removeClass("open");
       $("html").css("overflow", "auto");
-      $(".nav-menu").slideUp(500);
     }
   });
 });
